04120502d8c2b14b2cd3cc9e058943649ce7ecb3
Joshua Duffy <Josh.duffy30@gmail.com>
2020-08-03 08:13:07 -0700

    js cards

--- fetchbacon.js
+++ fetchbacon.js
@@ -1,2 +1,8 @@
-fetch(' https://baconipsum.com/api/?type=meat-and-filler')
-.then(res => console.log(res))
+function grabbacon() {
+  const targetElement = document.getElementById( elementid= 'target')
+
+    fetch(input ' https://baconipsum.com/api/?type=meat-and-filler')
+    .then(function (response: response). json() {
+
+    })
+}
